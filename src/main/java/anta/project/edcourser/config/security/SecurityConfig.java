2375ab2472fc577161412ff684aa620f968079e6
Denny Phantom <dennyphantom@protonmail.com>
2021-01-17 17:23:14 +0200

    add spring security config

--- src/main/java/anta/project/edcourser/config/security/SecurityConfig.java
+++ src/main/java/anta/project/edcourser/config/security/SecurityConfig.java
@@ -1,9 +1,14 @@
 package anta.project.edcourser.config.security;
 
+import anta.project.edcourser.config.security.token.jwt.JwtConfigurer;
 import anta.project.edcourser.config.security.token.jwt.JwtTokenProvider;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,11 +27,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String LOGIN_ENDPOINT = "/api/authorization";
     private static final String REGISTRATION_ENDPOINT = "/api/registration";
     private static final String RESTORE_PASSWORD = "/api/restore_password/**";
+    private static final String PUBLIC_API = "/api/public/**";
 
     @Autowired
     public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(LOGIN_ENDPOINT).permitAll()
+                .antMatchers(REGISTRATION_ENDPOINT).permitAll()
+                .antMatchers(RESTORE_PASSWORD).permitAll()
+                .antMatchers(PUBLIC_API).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+    }
 }
